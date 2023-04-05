7e0dc34f21a1fbdd850c3a2eeb1ebad3744e16f7
sripadaani <61870593+sripadaani@users.noreply.github.com>
2023-04-05 19:04:32 +0530

    Update variables.tf

--- Infra_files/variables.tf
+++ Infra_files/variables.tf
@@ -1,25 +1,36 @@
-variable "rgname"{
-    type = string
-    description = "used for naming resource group"
-}
-
-variable "rglocation"{
-    type = string
-    description = "used for selecting the location"
-    default = "eastus"    
-}
-
-variable "prefix"{
-    type = string
-    description = "used to definea standard prefix for all resource"
-}
-
-variable "vnet_cidr_prefix" {
+variable "azure_subscription_id" {
   type = string
-  description = "This variable defines address space for vnet"
 }
-
-variable "subnet1_cidr_prefix" {
+variable "azure_tenant_id" {
   type = string
-  description = "This variable defines address space for subnetnet"
+}
+variable "azure_client_id" {
+  type = string
+}
+variable "azure_client_secret" {
+  type = string
+}
+variable "azure_location" {
+  type = string
+}
+variable "azure_rg_name" {
+  type = string
+}
+variable "azure_vnet_name" {
+  type = string
+}
+variable "azure_subnet_name" {
+  type = string
+}
+variable "address_space" {
+  type = string
+}
+variable "address_prefixes" {
+  type = string
+}
+variable "azure_nic" {
+  type = string
+}
+variable "azure_vm" {
+  type = string
 }
